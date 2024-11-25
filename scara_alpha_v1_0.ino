517611bb69adc9bc15ce85108d69b163091f41f0
kedsuzuki <kedsuzuki820@gmail.com>
2024-11-25 00:18:27 -0500

    Init unit test script and updated void loop for alpha

--- scara_alpha_v1_0.ino
+++ scara_alpha_v1_0.ino
@@ -1,8 +1,8 @@
 /*
 @file: scara_alpha_v1_0.ino
-@updated: 2024/11/16
+@updated: 2024/11/24
 @author: Kensei Suzuki & Diego Gomez
-@brief: Alpha version meant for development. Top level main code for controlling the SCARA robot.
+@brief: Alpha version meant for development. Top level main code for controlling the SCARA robot. 
 */
 
 
@@ -12,6 +12,8 @@
  * ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 */
 #include <AccelStepper.h>
+#include <Servo.h>
+#include <math.h>
 
 #define NEMA17_MAX_SPEED        500.0     //steps per second
 #define NEMA17_MAX_ACCEL        50.0      //steps per second^2
@@ -31,6 +33,9 @@ const int BOTTOM_LIMIT_PIN    = 7;
 const int LEFT_LIMIT_PIN      = 8;
 const int RIGHT_LIMIT_PIN     = 9;
 const int GRIPPER_PIN         = 11;       //gripper servo motor PWM pin
+
+// globals
+int top_lim_step, bottom_lim_step, left_lim_step, right_lim_step;
 
 //class objects
 AccelStepper BaseStepper(AccelStepper::DRIVER, BASE_STEP_PIN, BASER_DIR_PIN);
@@ -75,7 +80,21 @@ void setup(){
  * ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 */
 void loop(){
-    
+    //get user input for each joint 
+    int base_position = getManualInput();
+    int z_position = getManualInput();
+    int elbow_position = getManualInput();
+
+    int b_step = calculateBaseStep(base_position);
+    int z_step = calculateZStep(z_position);
+    int e_step = calculateElbowStep(elbow_position);
+
+    moveAxis(&BaseStepper, b_step);
+    moveAxis(&ZStepper, z_step);
+    moveAxis(&ElbowStepper, e_step);
+
+    delay(500);
+
 } //end of loop()
 
 /*
@@ -86,20 +105,55 @@ void loop(){
 
 void calibrateLimits(){
   //top
-  
+  while(1){
+    digitalRead(TOP_LIMIT_PIN) == LOW ? break : void()0
+    ZStepper.setSpeed(NEMA17_MAX_SPEED / SPEED_REDUCTION);
+    ZStepper.runSpeed();
+  }
+  top_lim_step = ZStepper.currentPosition();
+
   //bottom
-
-  //left
-
-  //right
+  while(1){
+    digitalRead(BOTTOM_LIMIT_PIN) == LOW ? break : void()0
+    ZStepper.setSpeed(NEMA17_MAX_SPEED / 10);
+    ZStepper.runSpeed();
+  }
+  bottom_lim_step = ZStepper.currentPosition();
   
+  //left
+  while(1){
+    digitalRead(LEFT_LIMIT_PIN) == LOW ? break : void()0
+    ElbowStepper.setSpeed(NEMA17_MAX_SPEED / 10);
+    ElbowStepper.runSpeed();
+  }
+  top_lim_step = ElbowStepper.currentPosition();
+  
+  //right
+  while(1){
+    digitalRead(RIGHT_LIMIT_PIN) == LOW ? break : void()0
+    ElbowStepper.setSpeed(NEMA17_MAX_SPEED / 10);
+    ElbowStepper.runSpeed();
+  }
+  top_lim_step = ElbowStepper.currentPosition();
 }
 
 void zeroHome(){
+  ZStepper.moveTo( floor((top_lim_step+bottom_lim_step)/2) );
+  ZStepper.run();
+  ElbowStepper.moveTo( (floor((right_lim_step+left_lim_step)/2)) );
+  ElbowStepper.run();
 }
-  
 
-}
+// void moveAxis(&Stepper motor, float pos){
+  // todo/ks: generalized funtion to move any of the joints by passing a reference to the stepper object
+  // and the position to be moved to
+// }
 
-void moveAxis(&Stepper motor, float pos){
+int getManualInput(){
+  Serial.println("Enter position to move to: ");
+
+  while( Serial.available() == 0 ){}
+
+  return Serial.parseFloat();
+
 }
---
+++ scara_unit_test.ino
@@ -0,0 +1,88 @@
+/*
+@file: scara_unit_test.ino
+@updated: 2024/11/24
+@author: Kensei Suzuki & Diego Gomez
+@brief: Unit tests to check functionality of individual systems of the SCARA robot.
+Modify boolean definitions (0 or 1) to change which test to run. 
+*/
+
+
+/*
+ * vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+ *  LIBRARIES | GLOBALS | OBJECTS | FUNCTION PROTOTYPES
+ * ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+*/
+
+#define TEST_STEPPER      1       //moves one stepper using pins 7 and 8. Rotates left and right.
+#define TEST_SERVO        0
+#define TEST_LIMITSWITCH  0
+
+#include <AccelStepper.h>
+#include <Servo.h>
+
+#define NEMA17_MAX_SPEED        500.0     //steps per second
+#define NEMA17_MAX_ACCEL        50.0      //steps per second^2
+#define NEMA17_STEPS_PER_REV    200       //steps per one output revolution
+#define SPEED_REDUCTION         5         //reduce nema speed by this factor
+#define SPIN_DIRECTION          1         //1 or -1 for CW or CCW
+
+const int DIR_PIN           = 7;        //nema stepper motors control pins
+const int STEP_PIN          = 8;
+const int LIMIT_SWITCH_PIN  = 5;        //limit switch 
+const int SERVO_PIN         = 11;       //gripper servo motor PWM pin
+
+AccelStepper MyStepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
+Servo MyServo;
+
+/*
+ * vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+ *          SETUP - COMMS, ACTUATORS, SENSORS
+ * ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+*/
+void setup() {
+  MyStepper.setMaxSpeed(NEMA17_MAX_SPEED);
+  MyStepper.setAcceleration(NEMA17_MAX_ACCEL);
+  MyStepper.setSpeed(0);
+  MyStepper.runSpeed();
+  
+  pinMode(LIMIT_SWITCH_PIN, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  
+  MyServo.attach(SERVO_PIN);
+  MyServo.write(0);
+
+  delay(100);
+}
+
+/*
+ * vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+ *                    LOOP FUNCTION
+ * ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+*/
+void loop() {
+  if(TEST_STEPPER){
+    MyStepper.setSpeed(SPIN_DIRECTION * NEMA17_MAX_SPEED / SPEED_REDUCTION);
+    MyStepper.runSpeed();
+  }
+
+  if(TEST_LIMITSWITCH){
+    if(digitalRead(LIMIT_SWITCH_PIN) == LOW ){
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
+    else(digitalWrite(LED_BUILTIN, LOW));
+  }
+  
+  if(TEST_SERVO){
+    for (int cw = 0; cw <= 180; cw += 1) { 
+      // goes from 0 degrees to 180 degrees
+      MyServo.write(cw);         
+      delay(15);
+    }                 
+    for (int ccw = 180; ccw >= 0; ccw -= 1) { 
+      // goes from 180 degrees to 0 degrees
+      MyServo.write(ccw);              
+      delay(15);                    
+    }   
+  }
+}
